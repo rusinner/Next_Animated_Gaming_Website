54b7c04c7257ea4e1a7cd19c15df766b46bba7ed
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-25 17:24:21 +0200

    fixed tailwind issue
    uninstall and re install prefixer

--- gaming-website/app/page.jsx
+++ gaming-website/app/page.jsx
@@ -1,5 +1,4 @@
 import { Footer, Navbar } from "../components";
-
 import {
   About,
   Explore,
--- gaming-website/components/Navbar.jsx
+++ gaming-website/components/Navbar.jsx
@@ -12,11 +12,19 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50] inset-0 gradient-01" />
+    <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
         src="/search.svg"
-        alt="search icon"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+        Gaming World
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
         className="w-[24px] h-[24px] object-contain"
       />
     </div>

deb6109365c60f707869165b12c36a7c0637db40
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-25 17:30:22 +0200

    title and font changed

--- gaming-website/app/layout.jsx
+++ gaming-website/app/layout.jsx
@@ -3,7 +3,13 @@ import "../styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link
+          rel="stylesheet"
+          href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );

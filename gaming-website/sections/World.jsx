e03fab9bb14497632ab4a4ba20d4914d0ca0d4f1
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-26 16:59:49 +0200

    Insights section

--- gaming-website/sections/World.jsx
+++ gaming-website/sections/World.jsx
@@ -1,5 +1,50 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText, TitleText } from "../components";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+
 const World = () => {
-  return <div>World</div>;
+  return (
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col `}
+    >
+      <TypingText title={"| People on the World"} textStyles="text-center" />
+      <TitleText
+        textStyles="text-center"
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+      />
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src="people-01.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src="people-03.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default World;

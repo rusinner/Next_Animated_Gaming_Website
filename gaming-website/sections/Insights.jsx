a06e2870f6d3413bd34e7bdee6dc99180271db18
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-27 11:31:41 +0200

    Footer

--- gaming-website/sections/Insights.jsx
+++ gaming-website/sections/Insights.jsx
@@ -9,25 +9,27 @@ import { insights } from "@/constants";
 
 const Insights = () => {
   return (
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col `}
-    >
-      <TypingText title="| Insight" textStyles="text-center" />
-      <TitleText title="Insight about Gamverse" textStyles="text-center" />
-      <div className="mt-[30px] flex flex-col gap-[30px] ">
-        {insights.map((insight, index) => (
-          <InsightCard
-            key={`insight-${index}`}
-            {...insight}
-            index={index + 1}
-          />
-        ))}
-      </div>
-    </motion.div>
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: "false", amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col `}
+      >
+        <TypingText title="| Insight" textStyles="text-center" />
+        <TitleText title="Insight about Gamverse" textStyles="text-center" />
+        <div className="mt-[30px] flex flex-col gap-[30px] ">
+          {insights.map((insight, index) => (
+            <InsightCard
+              key={`insight-${index}`}
+              {...insight}
+              index={index + 1}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
   );
 };
 

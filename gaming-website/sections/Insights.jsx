41056b576feda951cc970aea8f636b796bdde192
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-27 11:09:16 +0200

    Feedback section

--- gaming-website/sections/Insights.jsx
+++ gaming-website/sections/Insights.jsx
@@ -1,5 +1,34 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText, TitleText, InsightCard } from "../components";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { insights } from "@/constants";
+
 const Insights = () => {
-  return <div>Insights</div>;
+  return (
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col `}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title="Insight about Gamverse" textStyles="text-center" />
+      <div className="mt-[30px] flex flex-col gap-[30px] ">
+        {insights.map((insight, index) => (
+          <InsightCard
+            key={`insight-${index}`}
+            {...insight}
+            index={index + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
+  );
 };
 
 export default Insights;

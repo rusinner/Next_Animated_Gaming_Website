60211b581b637de760b0693f3d72ee51ef18ada8
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-27 10:34:56 +0200

    Insights section

--- gaming-website/sections/World.jsx
+++ gaming-website/sections/World.jsx
@@ -27,9 +27,13 @@ const World = () => {
       />
       <motion.div
         variants={fadeIn("up", "tween", 0.3, 1)}
-        className="relative mt-[68px] flex w-full h-[550px]"
+        className="relative mt-[68px] flex w-full h-auto"
       >
-        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+        <img
+          src="/map.png"
+          alt="map"
+          className="w-[full] h-[full] object-cover"
+        />
 
         <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-01.png" alt="people" className="w-full h-full" />
